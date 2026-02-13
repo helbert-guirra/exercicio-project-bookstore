4564a371a786ca248545010e4b0de0f8cb547342
Helbert guirra <helbert.guirra@gmail.com>
2026-02-13 08:55:40 -0300

    correcao do viewsetProduct e teste para remocao de autenticação

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -1,11 +1,9 @@
 import json
 
 from django.urls import reverse
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 from rest_framework.views import status
 
-from order.factories import UserFactory
 from product.factories import CategoryFactory, ProductFactory
 from product.models import Product
 
@@ -14,20 +12,12 @@ class TestProductViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
-        self.user = UserFactory()
-        Token.objects.create(user=self.user)
-
         self.product = ProductFactory(
             title="pro controller",
             price=200.00,
         )
 
     def test_get_all_product(self):
-        token = Token.objects.get(user=self.user)
-        self.client.credentials(
-            HTTP_AUTHORIZATION="Token " + token.key
-        )
-
         response = self.client.get(
             reverse("product-list", kwargs={"version": "v1"})
         )
@@ -40,21 +30,8 @@ class TestProductViewSet(APITestCase):
             product_data["results"][0]["title"],
             self.product.title
         )
-        self.assertEqual(
-        float(product_data["results"][0]["price"]),
-        float(self.product.price)
-        )
-        self.assertEqual(
-            product_data["results"][0]["active"],
-            self.product.active
-        )
 
     def test_create_product(self):
-        token = Token.objects.get(user=self.user)
-        self.client.credentials(
-            HTTP_AUTHORIZATION="Token " + token.key
-        )
-
         category = CategoryFactory()
 
         data = json.dumps({
